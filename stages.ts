38cdcc3b46b2cb7f1af62f4e119301e618dbf23f
Stancia94 <staskobenko88@gmail.com>
2025-06-26 19:49:43 +0300

    commit

--- stages.ts
+++ stages.ts
@@ -3,22 +3,21 @@ type size = 'small' | 'middle' | 'large';
 function sizeBallInNumber(size: size) {
   switch (size) {
     case 'small':
-      return 2;
+      return 1 / 2;
     case 'middle':
-      return 1.5
+      return 1 / 3;
     case 'large':
-      return 0.2;
+      return 1 / 5;
   }
 }
-
 function countFloor(stage: number, size: size): number {
   const sizeBall = sizeBallInNumber(size);
   let counter = stage;
   while (stage > 1) {
-    stage /= sizeBall;
+    stage *= sizeBall;
     counter += Math.floor(stage);
   }
   return counter;
 }
 
-console.log(countFloor(10, "small"));
+console.log(countFloor(10, "large"));
